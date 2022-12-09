c62257d4ceb0d2bedd598d5d39c85cff05afa2f4
NikDoe <nikd0e@yandex.ru>
2022-12-09 18:15:20 +0300

    add usersApiSlice CRUD mutations

--- src/features/users/usersApiSlice.js
+++ src/features/users/usersApiSlice.js
@@ -29,21 +29,53 @@ export const usersApiSlice = apiSlice.injectEndpoints({
 				} else return [{ type: 'User', id: 'LIST' }];
 			},
 		}),
+		addNewUser: builder.mutation({
+			query: initialUserData => ({
+				url: '/users',
+				method: 'POST',
+				body: {
+					...initialUserData,
+				},
+			}),
+			invalidatesTags: [{ type: 'User', id: 'LIST' }],
+		}),
+		updateUser: builder.mutation({
+			query: initialUserData => ({
+				url: '/users',
+				method: 'PATCH',
+				body: {
+					...initialUserData,
+				},
+			}),
+			invalidatesTags: (result, error, arg) => [{ type: 'User', id: arg.id }],
+		}),
+		deleteUser: builder.mutation({
+			query: ({ id }) => ({
+				url: `/users`,
+				method: 'DELETE',
+				body: { id },
+			}),
+			invalidatesTags: (result, error, arg) => [{ type: 'User', id: arg.id }],
+		}),
 	}),
 });
 
-export const { useGetUsersQuery } = usersApiSlice;
+export const {
+	useGetUsersQuery,
+	useAddNewUserMutation,
+	useUpdateUserMutation,
+	useDeleteUserMutation,
+} = usersApiSlice;
 
 export const selectUsersResult = usersApiSlice.endpoints.getUsers.select();
 
 const selectUsersData = createSelector(
 	selectUsersResult,
-	usersResult => usersResult.data, // normalized state object with ids & entities
+	usersResult => usersResult.data,
 );
 
 export const {
 	selectAll: selectAllUsers,
 	selectById: selectUserById,
 	selectIds: selectUserIds,
-	// Pass in a selector that returns the users slice of state
 } = usersAdapter.getSelectors(state => selectUsersData(state) ?? initialState);
