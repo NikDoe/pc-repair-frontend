0e46636acd857783750899ca9bcd6b42b4164bd2
NikDoe <nikd0e@yandex.ru>
2022-12-11 19:22:32 +0300

    return compare func to create entity adapter & set polling

--- src/features/notes/NotesList.js
+++ src/features/notes/NotesList.js
@@ -9,7 +9,7 @@ const NotesList = () => {
 		isError,
 		error,
 	} = useGetNotesQuery(undefined, {
-		pollingInterval: 15000,
+		pollingInterval: 60000,
 		refetchOnFocus: true,
 		refetchOnMountOrArgChange: true,
 	});
