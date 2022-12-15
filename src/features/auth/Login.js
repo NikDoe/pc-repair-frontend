d6d28d4acb4b1a7399e18bde7ffaf3f14fdd7785
NikDoe <nikd0e@yandex.ru>
2022-12-15 17:50:54 +0300

    add logout button
    -refactor(Login.js): refactor titles

--- src/features/auth/Login.js
+++ src/features/auth/Login.js
@@ -57,7 +57,7 @@ const Login = () => {
 	return (
 		<section className="public">
 			<header>
-				<h1>Employee Login</h1>
+				<h1>Вход в кабинет работника</h1>
 			</header>
 			<main className="login">
 				<p ref={errRef} className={errClass} aria-live="assertive">
@@ -90,7 +90,7 @@ const Login = () => {
 				</form>
 			</main>
 			<footer>
-				<Link to="/">Back to Home</Link>
+				<Link to="/">Вернуться на главную</Link>
 			</footer>
 		</section>
 	);
