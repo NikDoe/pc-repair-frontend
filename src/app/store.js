d10eb8666ba57271a75cdc5d873ec68047995b56
NikDoe <nikd0e@yandex.ru>
2022-12-19 18:25:57 +0300

    disable dev mode for redux & react devtools

--- src/app/store.js
+++ src/app/store.js
@@ -1,7 +1,7 @@
 import { configureStore } from '@reduxjs/toolkit';
 import { apiSlice } from './api/apiSlice';
 import { setupListeners } from '@reduxjs/toolkit/query';
-import authReducer from '../features/auth/authSlice'
+import authReducer from '../features/auth/authSlice';
 
 export const store = configureStore({
 	reducer: {
@@ -9,7 +9,7 @@ export const store = configureStore({
 		auth: authReducer,
 	},
 	middleware: getDefaultMiddleware => getDefaultMiddleware().concat(apiSlice.middleware),
-	devTools: true,
+	devTools: false,
 });
 
 setupListeners(store.dispatch);
